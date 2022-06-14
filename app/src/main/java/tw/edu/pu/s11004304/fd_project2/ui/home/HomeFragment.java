1355a1dd757ee2df522031d7a36eaaadf49a8eb7
ciowlan <joanna071878@gmail.com>
2022-06-14 11:48:09 +0800

    Initial commit

--- app/src/main/java/tw/edu/pu/s11004304/fd_project2/ui/home/HomeFragment.java
+++ app/src/main/java/tw/edu/pu/s11004304/fd_project2/ui/home/HomeFragment.java
@@ -1,20 +1,34 @@
 package tw.edu.pu.s11004304.fd_project2.ui.home;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import java.util.ArrayList;
+
+import tw.edu.pu.s11004304.fd_project2.MainActivity;
+import tw.edu.pu.s11004304.fd_project2.R;
 import tw.edu.pu.s11004304.fd_project2.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    ViewPager vp_pager;
+    int prePosition;
+    LinearLayout ll_container;
+    ArrayList<ImageView> al;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +40,12 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        vp_pager = root.findViewById(R.id.vp_pager);
+        ll_container = root.findViewById(R.id.ll_container);
+        initData();
+        PollThread pThread = new PollThread();
+        pThread.start();
         return root;
     }
 
@@ -34,4 +54,105 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    class PollThread extends Thread{
+        @Override
+        public void run() {
+            boolean poll = true;
+            while (poll){
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        vp_pager.setCurrentItem(vp_pager.getCurrentItem()+1);
+                    }
+                });
+
+            }
+        }
+    }
+Thread mUiThread;
+    Handler mHandler = new Handler();
+    public final void runOnUiThread(Runnable action) {
+        if (Thread.currentThread() != mUiThread) {
+            mHandler.post(action);
+        } else {
+            action.run();
+        }
+    }
+
+    private void initData() {
+
+        int[] i = new int[]{R.drawable.eight, R.drawable.mysta, R.drawable.nine, R.drawable.mysta};
+        al = new ArrayList<ImageView>();
+        for (int x = 0; x < i.length; x++) {
+            ImageView iv = new ImageView(getContext());
+            iv.setBackgroundResource(i[x]);
+            al.add(iv);
+            View v = new View(getContext());
+            v.setBackgroundResource(R.drawable.point_normal);
+            //有多少張圖就放置幾個點
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(15, 15);
+            layoutParams.leftMargin = 30;
+            ll_container.addView(v, layoutParams);
+        }
+        vp_pager.setAdapter(new Myadapter());
+        vp_pager.setOnPageChangeListener(new MyPageListener());
+        vp_pager.setCurrentItem(al.size() * 1000);  //這個是無線輪詢的關鍵
+        ll_container.getChildAt(0).setBackgroundResource(R.drawable.point_select);
+        prePosition = 0;
+    }
+
+
+    class Myadapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return Integer.MAX_VALUE; // 要無限輪播
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            int position1 = position % al.size();
+            ImageView imageView = al.get(position1);
+            container.addView(imageView);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
+
+    class MyPageListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            int newPosition = position % al.size();
+            ll_container.getChildAt(newPosition).setBackgroundResource(R.drawable.point_select);
+            ll_container.getChildAt(prePosition).setBackgroundResource(R.drawable.point_normal);
+            prePosition=newPosition;
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    }
+
 }
--- app/src/main/java/tw/edu/pu/s11004304/fd_project2/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/tw/edu/pu/s11004304/fd_project2/ui/slideshow/SlideshowFragment.java
@@ -1,20 +1,59 @@
 package tw.edu.pu.s11004304.fd_project2.ui.slideshow;
 
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import tw.edu.pu.s11004304.fd_project2.R;
 import tw.edu.pu.s11004304.fd_project2.databinding.FragmentSlideshowBinding;
+
+
 
 public class SlideshowFragment extends Fragment {
 
     private FragmentSlideshowBinding binding;
+    RecyclerView rv;
+    ArrayList<SiteName> siteNames;
+    NetAdapter adapter;
+    Message msg;
+    Bundle bundle;
+    private ConstraintLayout mainLayout;
+    Handler handler = new Handler(Looper.getMainLooper());
+    LinearLayout layout;
+    ProgressBar pBar;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,12 +65,221 @@ public class SlideshowFragment extends Fragment {
 
         final TextView textView = binding.textSlideshow;
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        rv = root.findViewById(R.id.rv);
+        pBar = root.findViewById(R.id.p_bar);
+        pBar.setVisibility(View.VISIBLE);
+        mainLayout = root.findViewById(R.id.slidesLayout);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        // 設置格線
+        rv.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        GetWebInfo dTask = new GetWebInfo();
+        siteNames = new ArrayList<>();
+        dTask.execute("https://www.twtainan.net/data/shops_zh-tw.json");
         return root;
     }
 
+
+class GetWebInfo extends AsyncTask<String,Integer,String> {
+
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    protected void onPreExecute() {
+        pBar.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        pBar.setForegroundGravity(Gravity.CENTER);
+        pBar.setBackgroundColor(Color.GREEN);
+        pBar.setAlpha(0.7f);
+
+        //
+        pBar.setVisibility(View.VISIBLE);
+        //
+        Toast.makeText(getContext(), "開始下載 ...", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        for (int i = 0; i < 100; i++) {
+            try {
+                Thread.sleep(10);
+                pBar.setProgress(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String result = getData(strings[0]);
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String msg) {
+        pBar.setVisibility(View.GONE);
+        mainLayout.removeView(pBar);
+        super.onPostExecute(msg);
+        try {
+            JSONArray array = new JSONArray(msg);
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject site = array.getJSONObject(i);
+                //2-2
+                String name = site.getString("name");
+                String address = site.getString("address");
+                String district = site.getString("district");
+                String tel = site.getString("tel");
+                String open_time = site.getString("open_time");
+                String introduction = site.getString("introduction");
+
+                SiteName siteName = new SiteName(name, address, district, tel, open_time, introduction);
+                //
+                siteNames.add(siteName);
+
+
+            }
+        } catch (JSONException e) {
+            Log.v("tag", e.toString());
+            e.printStackTrace();
+        }
+        if (siteNames.size() != 0) {
+            adapter = new NetAdapter();
+            adapter.setmData(siteNames);
+            rv.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        pBar.setProgress(values[0]);
+        super.onProgressUpdate(values);
+    }
+
+}
+
+
+    public String getData(String strUrl){
+        StringBuffer sb = new StringBuffer();
+        try {
+            URL url = new URL(strUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            InputStream in = conn.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(reader);
+            String str;
+            while((str = br.readLine())!=null){
+                sb.append(str);
+            }
+
+            JSONArray array = new JSONArray(sb.toString());
+
+        } catch (Exception e) {
+            Log.v("tag",e.toString());
+            e.printStackTrace();
+        }finally {
+        }
+        return sb.toString();
+    }
+
+}
+
+
+
+class NetAdapter extends RecyclerView.Adapter <NetAdapter.VH>{
+
+    private List<SiteName> mData;
+
+
+    public  NetAdapter(){
+
+    }
+
+    public void setmData(List<SiteName> data){
+        mData = data;
+        Log.v("NetAdapter",mData.size()+"");
+    }
+
+    @NonNull
+    @Override
+    public NetAdapter.VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.myitem2, parent, false);
+        return new NetAdapter.VH(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NetAdapter.VH holder, int position) {
+        holder.tvName.setText(mData.get(position).getName());
+        holder.tvAddress.setText(mData.get(position).getAddress());
+        holder.tvTel.setText(mData.get(position).getTel());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    class VH extends RecyclerView.ViewHolder{
+        TextView tvName,tvAddress,tvDistrict,tvTel,tvOpen_time,tvIntroduction;
+        public VH(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvAddress = itemView.findViewById(R.id.tvAddress);
+            tvTel = itemView.findViewById(R.id.tvTel);
+
+        }
+    }
+
+
+}
+
+class SiteName implements Serializable {
+    public String name;
+    public String address;
+    public String district;
+    public String tel;
+    public String open_time;
+    public String introduction;
+
+    public SiteName(String name, String address, String district, String tel, String open_time, String introduction) {
+        this.name = name;
+        this.address = address;
+        this.district = district;
+        this.tel = tel;
+        this.open_time = open_time;
+        this.introduction = introduction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public String getOpen_time() {
+        return open_time;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    @Override
+    public String toString() {
+        return "SiteName{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", district='" + district + '\'' +
+                ", tel='" + tel + '\'' +
+                ", open_time='" + open_time + '\'' +
+                ", introduction='" + introduction + '\'' +
+                '}';
     }
 }
